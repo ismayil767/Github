3c7508d20416fa05ec8c3f7ddcc6494d6aec2d20
ismayil767 <ismayilhuseynov258@gmail.com>
2015-06-18 00:49:51 +0500

    Github2

---
+++ firstMVC/firstMVC/Controllers/haqqimizdaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstMVC.Controllers
+{
+    public class haqqimizdaController : Controller
+    {
+        //
+        // GET: /haqqimizda/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ firstMVC/firstMVC/Controllers/qeydiyyatController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstMVC.Controllers
+{
+    public class qeydiyyatController : Controller
+    {
+        //
+        // GET: /qeydiyyat/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
